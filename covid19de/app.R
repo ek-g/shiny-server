67a4096fad606075c1dd70d837c8548feedf846c
ek-g <eero@kuusisto.de>
2020-03-30 11:13:42 +0000

    Added new app

---
+++ covid19de/app.R
@@ -0,0 +1,217 @@
+library(shiny)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(ggplot2)
+library(googlesheets4)
+library(readr)
+library(plotly)
+library(shinydashboard)
+library(DT)
+
+datapath <- "data/risklayer.csv"
+
+risklayer <- "1wg-s4_Lz2Stil6spQEYFdZaBEp8nWW26gVyfHqvcl8s"
+
+today <- format(today(), "%e.%m.")
+
+sheets_deauth()
+
+if(date(file.info(datapath)$mtime) < today()){
+    
+    landdata_raw <- sheets_read(risklayer, sheet = "Curve2", range = cell_rows(31:47)) %>% 
+        select_if(~sum(!is.na(.)) > 0)
+    
+    bevoelk <- tibble(Bundesland = pull(sheets_read(risklayer, sheet = "Statistics", range = "B1:B17")),
+                      Bevoelkerung = pull(sheets_read(risklayer, sheet = "Statistics", range = "G1:G17")),
+                      !!today := pull(sheets_read(risklayer, sheet = "Statistics", range = "D1:D17")),
+                      .name_repair = "minimal")
+    
+    todesfaelle <- tibble(Bundesland = pull(sheets_read(risklayer, sheet = "Statistics", range = "B1:B17")),
+                      Tote = pull(sheets_read(risklayer, sheet = "Statistics", range = "I1:I17")),
+                      .name_repair = "minimal")
+    
+    landdata <- left_join(landdata_raw, bevoelk %>% select(-Bevoelkerung), "Bundesland")
+    
+    landdata <- landdata %>%
+        pivot_longer(-Bundesland, names_to = "Datum", values_to = "Infizierte") %>% 
+        mutate(Datum = as_date(Datum, tz = "Europe/Berlin", format = "%e.%m.")) %>%
+        arrange(Bundesland, Datum)
+    
+    landdata <- left_join(landdata, bevoelk %>% select(-!!today), "Bundesland")
+    
+    write_csv(landdata, datapath)
+    
+    }else landdata <- read_csv(datapath) 
+
+kuerzel <- tribble(~Kuerzel, ~Bundesland,
+                  "BW", "Baden-Württemberg",
+                  "BY", "Bayern",
+                  "BE", "Berlin",
+                  "BB", "Brandenburg",
+                  "HB", "Bremen",
+                  "HH", "Hamburg",
+                  "HE", "Hessen",
+                  "MV", "Mecklenburg-Vorpommern",
+                  "NI", "Niedersachsen",
+                  "NW", "Nordrhein-Westfalen",
+                  "RP", "Rheinland-Pfalz",
+                  "SL", "Saarland",
+                  "SN", "Sachsen",
+                  "ST", "Sachsen-Anhalt",
+                  "SH", "Schleswig-Holstein",
+                  "TH", "Thüringen")
+
+landdata <- left_join(landdata, kuerzel, "Bundesland")
+
+todesfaelle <- tibble(Bundesland = pull(sheets_read(risklayer, sheet = "Statistics", range = "B1:B17")),
+                      Tote = pull(sheets_read(risklayer, sheet = "Statistics", range = "I1:I17")),
+                      .name_repair = "minimal")
+
+gesamtzahl <- pull(sheets_read(risklayer, sheet = "Statistics", range = "D18", col_names = "Anzahl"))
+
+todesfaelle <- left_join(todesfaelle, kuerzel, "Bundesland")
+
+fallmax <- max(landdata$Infizierte, na.rm = TRUE)
+    
+gesamt <- landdata %>% 
+  filter(Datum == max(Datum)) %>% 
+  summarise(Infizierte = sum(Infizierte)) %>% 
+  pull()
+    
+gesamt_bevoelkerung <- landdata %>% 
+  distinct(Bundesland, .keep_all = T) %>% 
+  summarise(Bevoelkerung = sum(Bevoelkerung)) %>% 
+  pull()
+    
+datum <- format(max(landdata$Datum), "%d.%m.%Y")
+
+ui <- dashboardPage(skin = "red",
+  dashboardHeader(title = "Covid-19"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Data", icon = icon("table"), tabName = "data")
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem("dashboard",
+    fluidRow(
+        column(8,
+             box(plotlyOutput("landplot", height = "685px"), width = NULL)
+              ),
+        column(4,
+               fluidRow(
+                    column(6,
+                        valueBox(paste0(gesamtzahl), paste0("Infizierte (", datum, ")"),
+                                    icon = icon("bug"),
+                                    width = NULL,
+                                    color = "red")
+                            ),
+                    column(6,
+                        valueBox(format(gesamt / gesamt_bevoelkerung * 1000, digits = 4),
+                                 "pro 1000 Einwohner",
+                                 icon = icon("bug"),
+                                 width = NULL,
+                                 color = "red")
+                            )
+                    ),
+               valueBox(sum(todesfaelle$Tote), paste0("Todesfälle (", datum, ")"),
+                        icon = icon("bug"),
+                        width = NULL,
+                        color = "black"),
+               tabBox(
+                 #title = paste("Stand:", datum),
+                 tabPanel("Infizierte pro 1000", plotlyOutput("relplot"), width = NULL),
+                 tabPanel("Todesfälle", plotlyOutput("toteplot"), width = NULL),
+                 width= NULL)
+               )
+            ),
+    fluidRow(column(12, 
+                box(p("Letzte Aktualisierung am ", file.info(datapath)$mtime, "- Daten: ", a("Risklayer", href="http://www.risklayer.com/de/")),
+                 p("©", a("Eero Kuusisto-Gussmann", href="http://kuusisto.de")),
+                 width = NULL
+    )))
+        ),
+      tabItem("data",
+              fluidRow(
+              column(12, box(DTOutput('datatable', width = NULL)))
+              )
+              )
+      )
+  )
+)
+
+server <- function(input, output) {
+
+    output$landplot <- renderPlotly({
+        
+        plot_ly(landdata, x =~Datum, y =~Infizierte,
+                color =~Bundesland, type='scatter', mode = 'lines',
+                line = list(width = 4),
+                text = ~Bundesland,
+                hovertemplate = paste(
+                    "<b>%{text}</b><br>",
+                    "%{yaxis.title.text}: %{y:d}<br>",
+                    "%{xaxis.title.text}: %{x}",
+                    "<extra></extra>")
+                ) %>% 
+            layout(
+                yaxis = list(
+                    range = c(0, fallmax)),
+                xaxis = list(
+                    type = 'date',
+                    tickformat = "%d.%m.%Y"),
+                legend = list(
+                  x = 0.02,
+                  y = 0.98,
+                  font = list(size = 8)
+                )
+                )
+    })
+    
+    output$relplot <- renderPlotly({
+        
+        landdata %>% 
+            filter(Datum == max(Datum)) %>% 
+            mutate(pro1000 = Infizierte / Bevoelkerung *1000) %>% 
+            arrange(pro1000) %>% 
+            plot_ly(x = ~pro1000, y = ~Kuerzel,
+                    type = 'bar', color = ~pro1000,
+                    colors = "YlOrRd", orientation = 'h',
+                    text = ~Bundesland,
+                    hovertemplate = paste(
+                        "<b>%{text}</b><br>",
+                        "%{x:.2f} %{xaxis.title.text}",
+                        "<extra></extra>")) %>% 
+            layout(yaxis = list(categoryorder = "array", categoryarray = ~pro1000,
+                                title = "Bundesland"),
+                   xaxis = list(title = "Infizierte pro 1000 Einwohner")) %>%
+            hide_colorbar()
+    })
+    
+    output$toteplot <- renderPlotly({
+        
+        todesfaelle %>% 
+            arrange(Tote) %>% 
+            plot_ly(x = ~Tote, y = ~Kuerzel,
+                    type = 'bar', color = ~Tote,
+                    colors = "YlOrRd", orientation = 'h',
+                    text = ~Bundesland,
+                    hovertemplate = paste(
+                        "<b>%{text}</b><br>",
+                        "%{x:d} Todesfälle<br>",
+                        "<extra></extra>")) %>% 
+            layout(yaxis = list(categoryorder = "array", categoryarray = ~Tote,
+                                title = "Bundesland"),
+                   xaxis = list(title = "Todesfälle pro Bundesland")) %>%
+            hide_colorbar()
+    })
+    
+    output$datatable = renderDT(
+      landdata %>% select(-Bevoelkerung), options = list(lengthChange = FALSE)
+    )
+}
+
+shinyApp(ui = ui, server = server)
