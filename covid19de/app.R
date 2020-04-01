4a571a5f041bf76c366def180eeee060c5cf4f53
ek-g <eero@kuusisto.de>
2020-04-01 12:53:02 +0200

    Fixed an issue with server time zone, added new plots and relative numbers

--- covid19de/app.R
+++ covid19de/app.R
@@ -13,7 +13,13 @@ datapath <- "data/risklayer.csv"
 
 risklayer <- "1wg-s4_Lz2Stil6spQEYFdZaBEp8nWW26gVyfHqvcl8s"
 
-today <- format(today(), "%e.%m.")
+tzone <- "Europe/Berlin"
+
+Sys.setenv(TZ = "Europe/Berlin")
+
+today <- format(today(tzone = tzone), "%d.%m.")
+
+today_format <- format(today(tzone = tzone), "%d.%m.%Y")
 
 sheets_deauth()
 
@@ -26,13 +32,13 @@ todaystats <- sheets_read(risklayer, sheet = "Statistics", range = "B1:I17")  %>
 heute <- todaystats %>% 
   select(1,3) %>% 
   pivot_longer(-Bundesland, names_to = "Datum", values_to = "Infizierte") %>% 
-  mutate(Datum = as_date(Datum, tz = "Europe/Berlin", format = "%e.%m.")) %>%
+  mutate(Datum = as_date(Datum, tz = tzone, format = "%d.%m.")) %>%
   arrange(Bundesland, Datum)
 
 bevoelk <- todaystats %>% 
   select(1, 6)
 
-gesamtzahl <- pull(sheets_read(risklayer, sheet = "Statistics", range = "D18", col_names = "Anzahl"))
+gesamtzahl <- sum(todaystats %>% select(!!today))
 
 kuerzel <- tribble(~Kuerzel, ~Bundesland,
                    "BW", "Baden-Württemberg",
@@ -52,14 +58,14 @@ kuerzel <- tribble(~Kuerzel, ~Bundesland,
                    "SH", "Schleswig-Holstein",
                    "TH", "Thüringen")
 
-if(date(file.info(datapath)$mtime) < today()){
+if(date(file.info(datapath)$mtime) < today(tzone = tzone)){
     
     landdata_raw <- sheets_read(risklayer, sheet = "Curve2", range = cell_rows(31:47)) %>% 
         select_if(~sum(!is.na(.)) > 0)
     
     landdata <- landdata_raw %>%
         pivot_longer(-Bundesland, names_to = "Datum", values_to = "Infizierte") %>% 
-        mutate(Datum = as_date(Datum, tz = "Europe/Berlin", format = "%e.%m.")) %>%
+        mutate(Datum = as_date(Datum, tz = tzone, format = "%e.%m.")) %>%
         arrange(Bundesland, Datum)
     
     write_csv(landdata, datapath)
@@ -68,11 +74,15 @@ if(date(file.info(datapath)$mtime) < today()){
 
 landdata <- left_join(landdata, kuerzel, "Bundesland")
 
-landdata <- left_join(landdata, heute)
+landdata <- full_join(landdata, heute, by = c("Bundesland", "Datum", "Infizierte")) %>% 
+  arrange(Bundesland, Datum)
 
 landdata <- left_join(landdata, bevoelk, "Bundesland")
 
-todesfaelle <- left_join(todaystats %>% select(Bundesland, Tote), kuerzel, "Bundesland")
+heute <- left_join(heute, bevoelk) %>% 
+  left_join(kuerzel)
+
+todesfaelle <- left_join(todaystats %>% select(Bundesland, Tote, Bevoelkerung), kuerzel, "Bundesland")
 
 fallmax <- max(landdata$Infizierte, na.rm = TRUE)
     
@@ -96,49 +106,65 @@ ui <- dashboardPage(skin = "red",
       tabItem("dashboard",
     fluidRow(
         column(8,
-             box(plotlyOutput("landplot", height = "685px"), width = NULL)
+            tabBox(
+                  tabPanel("Infizierte",
+                           plotlyOutput("landplot", height = "650px"), width = NULL),
+                  tabPanel("Infizierte pro 100 Tsd. Einwohner",
+                           plotlyOutput("rellandplot", height = "650px"), width = NULL),
+                  width = NULL)
               ),
         column(4,
                fluidRow(
                     column(6,
-                        valueBox(paste0(gesamtzahl), paste0("Infizierte (", datum, ")"),
+                        valueBox(paste0(gesamtzahl), paste0("Infizierte (", today_format, ")"),
                                     icon = icon("bug"),
                                     width = NULL,
                                     color = "red")
                             ),
                     column(6,
-                        valueBox(format(gesamtzahl / gesamt_bevoelkerung * 1000, digits = 4),
-                                 "pro 1000 Einwohner",
+                        valueBox(format(gesamtzahl / gesamt_bevoelkerung * 100000, digits = 4),
+                                 "pro 100 Tsd. Einwohner",
                                  icon = icon("bug"),
                                  width = NULL,
                                  color = "red")
                             )
                     ),
-               valueBox(sum(todesfaelle$Tote), paste0("Todesfälle (", datum, ")"),
+               fluidRow(
+                    column(6, valueBox(sum(todesfaelle$Tote), paste0("Todesfälle (", today_format, ")"),
                         icon = icon("bug"),
                         width = NULL,
                         color = "black"),
+                          ),
+                    column(6, valueBox(format(sum(todesfaelle$Tote) / gesamt_bevoelkerung * 100000, digits = 6),
+                                       "pro 100 Tsd. Einwohner",
+                                       icon = icon("bug"),
+                                       width = NULL,
+                                       color = "black")
+                          )
+                        ),
                tabBox(
-                 #title = paste("Stand:", datum),
-                 tabPanel("Infizierte pro 1000", plotlyOutput("relplot"), width = NULL),
-                 tabPanel("Todesfälle", plotlyOutput("toteplot"), width = NULL),
+                 #title = "Relative Zahlen",
+                 tabPanel("Infizierte pro 100 Tsd.",
+                          plotlyOutput("relplot"), width = NULL),
+                 tabPanel("Todesfälle pro 100 Tsd.",
+                          plotlyOutput("toteplot"), width = NULL),
                  width= NULL)
-               )
-            ),
+                ),
+               ),
     fluidRow(column(12, 
-                box(p("Letzte Aktualisierung am ", file.info(datapath)$mtime, "- Daten: ", a("Risklayer", href="http://www.risklayer.com/de/")),
+                box(p("Letzte Aktualisierung am ", format(Sys.time(), tzone = tzone), "- Daten: ", a("Risklayer", href="http://www.risklayer.com/de/")),
                  p("©", a("Eero Kuusisto-Gussmann", href="http://kuusisto.de")),
                  width = NULL
-    )))
-        ),
+    )))),
       tabItem("data",
               fluidRow(
-              column(12, box(DTOutput('datatable', width = NULL)))
+              column(12, box(DTOutput('datatable'), width = NULL))
               )
               )
       )
   )
 )
+
 
 server <- function(input, output) {
 
@@ -168,11 +194,37 @@ server <- function(input, output) {
                 )
     })
     
+    output$rellandplot <- renderPlotly({
+      
+      plot_ly(landdata, x =~Datum, y =~Infizierte / Bevoelkerung * 100000,
+              color =~Bundesland, type='scatter', mode = 'lines',
+              line = list(width = 4),
+              text = ~Bundesland,
+              hovertemplate = paste(
+                "<b>%{text}</b><br>",
+                "%{y:.2f} %{yaxis.title.text}<br>",
+                "%{xaxis.title.text}: %{x}",
+                "<extra></extra>")
+      ) %>% 
+        layout(
+          yaxis = list(
+            range = c(0, max(landdata$Infizierte / landdata$Bevoelkerung * 100000)),
+            title = "Infizierte pro 100 Tsd. Einwohner"),
+          xaxis = list(
+            type = 'date',
+            tickformat = "%d.%m.%Y"),
+          legend = list(
+            x = 0.02,
+            y = 0.98,
+            font = list(size = 8)
+          )
+        )
+    })
+    
     output$relplot <- renderPlotly({
         
-        landdata %>% 
-            filter(Datum == max(Datum)) %>% 
-            mutate(pro1000 = Infizierte / Bevoelkerung *1000) %>% 
+        heute %>% 
+            mutate(pro1000 = Infizierte / Bevoelkerung * 100000) %>% 
             arrange(pro1000) %>% 
             plot_ly(x = ~pro1000, y = ~Kuerzel,
                     type = 'bar', color = ~pro1000,
@@ -184,30 +236,31 @@ server <- function(input, output) {
                         "<extra></extra>")) %>% 
             layout(yaxis = list(categoryorder = "array", categoryarray = ~pro1000,
                                 title = "Bundesland"),
-                   xaxis = list(title = "Infizierte pro 1000 Einwohner")) %>%
+                   xaxis = list(title = "Infizierte pro 100 Tsd. Einwohner")) %>%
             hide_colorbar()
     })
     
     output$toteplot <- renderPlotly({
         
         todesfaelle %>% 
-            arrange(Tote) %>% 
-            plot_ly(x = ~Tote, y = ~Kuerzel,
-                    type = 'bar', color = ~Tote,
+            mutate(pro1000 = Tote / Bevoelkerung * 100000) %>% 
+            arrange(pro1000) %>% 
+            plot_ly(x = ~pro1000, y = ~Kuerzel,
+                    type = 'bar', color = ~pro1000,
                     colors = "YlOrRd", orientation = 'h',
                     text = ~Bundesland,
                     hovertemplate = paste(
                         "<b>%{text}</b><br>",
-                        "%{x:d} Todesfälle<br>",
+                        "%{x:.2f} Todesfälle<br>",
                         "<extra></extra>")) %>% 
-            layout(yaxis = list(categoryorder = "array", categoryarray = ~Tote,
+            layout(yaxis = list(categoryorder = "array", categoryarray = ~pro1000,
                                 title = "Bundesland"),
-                   xaxis = list(title = "Todesfälle pro Bundesland")) %>%
+                   xaxis = list(title = "Todesfälle pro 100 Tsd. Einwohner")) %>%
             hide_colorbar()
     })
     
     output$datatable = renderDT(
-      landdata %>% select(-Bevoelkerung), options = list(lengthChange = FALSE)
+      landdata %>% select(-Bevoelkerung, -Kuerzel), options = list(lengthChange = FALSE, pageLength = 20, filter = 'top')
     )
 }
 
